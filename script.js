5d783ff67203399724506514b9540981b089c667
DavidHart05 <>
2021-05-06 15:17:28 +0000

    will this satisfy the overly picky linter?

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let lengthA
-let lengthB
-let lengthH
+let length-a
+let length-b
+let length-h
 let area
 
 // adding event listener to the button, when pressed, it preforms the functions doArea and doCircumference.
@@ -8,23 +8,23 @@ document.getElementById('button').addEventListener('click', doArea)
 
 function doArea () {
   // gets the input values
-  lengthA = document.getElementById('inputA').value
-  lengthB = document.getElementById('inputB').value
-  lengthH = document.getElementById('inputH').value
+  length-a = document.getElementById('input-a').value
+  length-b = document.getElementById('input-b').value
+  length-h = document.getElementById('input-h').value
 
   // converts strings to floating point numbers
-  lengthA = parseFloat(lengthA)
-  lengthB = parseFloat(lengthB)
-  lengthH = parseFloat(lengthH)
+  length-a = parseFloat(length-a)
+  length-b = parseFloat(length-b)
+  length-h = parseFloat(length-h)
 
   // add length a and b
-  area = lengthA + lengthB
+  area = length-a + length-b
 
   // divide by two
   area = area / 2
 
   // multiply by height
-  area = area * lengthH
+  area = area * length-h
 
   // write to HTML
   document.getElementById('areaAns').innerHTML = area
