801664882b250c1122051b8bc6bae4acca27f3cf
DavidHart05 <>
2021-05-06 15:30:24 +0000

    more

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let length_A
-let length_B
-let length_H
+let lengthA
+let lengthB
+let lengthH
 let area
 
 // adding event listener to the button, when pressed, it preforms the functions doArea and doCircumference.
@@ -8,23 +8,23 @@ document.getElementById('button').addEventListener('click', doArea)
 
 function doArea () {
   // gets the input values
-  length_A = document.getElementById('input-a').value
-  length_B = document.getElementById('input-b').value
-  length_H = document.getElementById('input-h').value
+  lengthA = document.getElementById('input-a').value
+  lengthB = document.getElementById('input-b').value
+  lengthH = document.getElementById('input-h').value
 
   // converts strings to floating point numbers
-  length_A = parseFloat(length_A)
-  length_B = parseFloat(length_B)
-  length_H = parseFloat(length_H)
+  lengthA = parseFloat(lengthA)
+  lengthB = parseFloat(lengthB)
+  lengthH = parseFloat(lengthH)
 
   // add length a and b
-  area = length_A + length_B
+  area = lengthA + lengthB
 
   // divide by two
   area = area / 2
 
   // multiply by height
-  area = area * length_H
+  area = area * lengthH
 
   // write to HTML
   document.getElementById('area-ans').innerHTML = area
